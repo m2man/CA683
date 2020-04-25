0b4a478b055d5bbf623a6643f0016db1145fa3f3
NGUYEN MANH DUY <nmduy881994@gmail.com>
2020-04-25 01:26:57 +0100

    update

--- Credit Project/Imputing.py
+++ Credit Project/Imputing.py
@@ -4,8 +4,9 @@ from fancyimpute import IterativeImputer as MICE
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv('data/filtered_merged_application_test.csv')
+data = pd.read_csv('data/merged_application_train.csv')
 data_remove_id = data.drop(['SK_ID_CURR'], axis=1) if 'SK_ID_CURR' in list(data.columns) else data.copy()
+data_remove_id = data_remove_id.drop(['TARGET'], axis=1) if 'TARGET' in list(data_remove_id.columns) else data_remove_id.copy()
 data_remove_id = data_remove_id.drop(['DAYS_EMPLOYED_ANOM'], axis=1) if 'DAYS_EMPLOYED_ANOM' in list(data_remove_id.columns) else data_remove_id.copy()
 data_remove_id = data_remove_id.drop(['Unnamed: 0'], axis=1) if 'Unnamed: 0' in list(data_remove_id.columns) else data_remove_id.copy()
 
@@ -17,4 +18,4 @@ imputed_pd = pd.DataFrame(data=imputed_app_train)
 imputed_pd.columns = list(encoded_data.columns)
 imputed_pd['TARGET'] = list(data['TARGET'])
 imputed_pd['SK_ID_CURR'] = list(data['SK_ID_CURR'])
-imputed_pd.to_csv('data/imputed_merged_test.csv', index=False)
+imputed_pd.to_csv('data/imputed_merged_train.csv', index=False)
